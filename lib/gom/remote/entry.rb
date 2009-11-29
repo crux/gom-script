9c9a6d3bb8dd7bb2f8aaf77b137b04fd94c6202f
dirk luesebrink <dirk@sebrink.de>
2009-11-29 02:29:58 +0100

    experimental: gnode part of Entry

--- lib/gom/remote/entry.rb
+++ lib/gom/remote/entry.rb
@@ -5,6 +5,18 @@ module Gom
       def connection 
         Gom::Remote.connection
       end
+
+      def gnode path
+        json = (connection.read "#{path}.json")
+        (JSON.parse json)["node"]["entries"].select do |entry|
+          # 1. select attribute entries
+          entry.has_key? "attribute"
+        end.inject({}) do |h, a|
+            # 2. make it a key, value list
+            h[a["attribute"]["name"].to_sym] = a["attribute"]["value"]
+            h
+        end
+      end
     end
   end
 end
